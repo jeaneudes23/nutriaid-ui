3517d8c21588b92fd97c900cee4712bce38bace1
Nihangaza Jean Eudes <121790290+jeaneudes23@users.noreply.github.com>
2026-08-17 21:16:02 +0200

    nAID for responsive logo

--- src/components/AppLogo.tsx
+++ src/components/AppLogo.tsx
@@ -4,7 +4,7 @@ export const AppLogo = () => {
   return (
     <span className="text-primary font-heading text-xl font-bold">
       <span className="hidden lg:inline">{CONFIG.app_name}</span>
-      <span className="inline lg:hidden">{CONFIG.app_name.slice(0, 4)}</span>
+      <span className="inline lg:hidden">{CONFIG.app_name[0] + CONFIG.app_name.slice(-3)}</span>
     </span>
   );
 };
