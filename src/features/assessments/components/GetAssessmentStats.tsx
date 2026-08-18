88ea89c6ee94d1b54f9b1d8d385b51d15071e865
Nihangaza Jean Eudes <121790290+jeaneudes23@users.noreply.github.com>
2026-08-18 08:44:35 +0200

    date bug fix

--- src/features/assessments/components/GetAssessmentStats.tsx
+++ src/features/assessments/components/GetAssessmentStats.tsx
@@ -11,7 +11,7 @@ interface Stat {
 
 export function getAssessmentStats(assessment: Assessment): Stat[] {
   return [
-    { title: "Age", icon: <UserIcon className="size-4" />, value: getAgeInYearsAndMonths(assessment.measuredAt, assessment.childId.dateOfBirth) },
+    { title: "Age", icon: <UserIcon className="size-4" />, value: `${assessment.ageMonthsAtMeasurement} months` },
     { title: "Measured At", icon: <CalendarIcon className="size-4" />, value: parseDate(assessment.measuredAt) },
     { title: "Weight", icon: <WeightIcon className="size-4" />, value: assessment.weightKg, unit: "kg" },
     { title: "Height", icon: <MoveVerticalIcon className="size-4" />, value: assessment.heightCm, unit: "cm" },
